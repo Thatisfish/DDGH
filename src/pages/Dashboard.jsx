38b5f130034b009a635e7cb9eccb587ffc58a1a5
Thatisfish <roobin4321@gmail.com>
2025-09-26 04:11:18 +0800

    0926成品

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/Dashboard.scss'
+import { GrNotes } from "react-icons/gr";
 
 const Dashboard = () => {
 	return (
 		<div className='dashboard'>
 			<Link to="/DecoctionTool" className="dashboard__block">
-				<img
-					src="https://via.placeholder.com/60"
-					alt="水藥試算"
-					className="dashboard__icon"
-				/>
+				<GrNotes size={50} />
 				<span className="dashboard__label">水藥試算</span>
 			</Link>
 		</div>
